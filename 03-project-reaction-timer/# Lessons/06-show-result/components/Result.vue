90c605d95e06e1ad146513b10061bdf588260768
Hung Bui <hungbui7690@gmail.com>
2024-10-06 12:35:52 -0600

    Project - Reaction Timer 1

---
+++ 03-project-reaction-timer/# Lessons/06-show-result/components/Result.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  data() {
+    return {
+      rank: null,
+    }
+  },
+  props: ['score'], // #
+  mounted() {
+    if (this.score < 250) {
+      this.rank = 'Ninja Fingers'
+    } else if (this.score < 400) {
+      this.rank = 'Rapid Reflexes'
+    } else {
+      this.rank = 'Snail pace...'
+    }
+  },
+}
+</script>
+
+<template>
+  <div>
+    <!-- # -->
+    <h3>Result - {{ score }}</h3>
+    <p>{{ rank }}</p>
+  </div>
+</template>
+
+<style scoped></style>
